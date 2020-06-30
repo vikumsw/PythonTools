2bc9161f69dc936e145a7f809f0024e4a4d91981
Vikum Wijesinghe <vikumsw@gmail.com>
2020-06-30 23:18:04 +0530

    Git Notebook Commiter - Remove comments
    Signed-off-by: Vikum Wijesinghe <vikumsw@gmail.com>

--- GitCommiter/git.py
+++ GitCommiter/git.py
@@ -1,25 +1,3 @@
-# import sys
-import os
-import subprocess
-
-# from DateGenerator import getDate, printDate
-'''
-args = sys.argv[1:]
-if len(args)!= 4:
-    print('Invalid Usage!!')
-    print('Correct Usage -> gitta.py <homeBranch> <targetBranch> <tagName> <tagMessage>')
-    print('Stopping')
-    quit()
-
-
-print('Number of arguments:', len(args), 'arguments.')
-print('Argument List:', str(args))
-
-print(getDate())
-printDate()
-'''
-
-
 def read_dates():
     f = open("Dates.txt", "r")
     for x in f:
@@ -51,4 +29,3 @@ for i in range(0, 21):
     subprocess.check_call(['git'] + ['commit', '-m', r'"Changes to Basic WorkFlow Notebook"'])
     dateComplete = '--date="' + dateList[i] +'"'
     subprocess.check_call(['git'] + ['commit', '--amend', '--no-edit', dateComplete])
-    #print(dateComplete)
