5e9446ffdde9d9da3097a593d15486d69004aabb
Vikum Wijesinghe <vikumsw@gmail.com>
2020-07-01 09:17:17 +0530

    Generalizing to more notebooks

--- GitCommiter/git.py
+++ GitCommiter/git.py
@@ -1,3 +1,11 @@
+import os
+import subprocess
+
+folderName = 'BasicFeatureVisualizations'
+fileTitle='basic-feature-visualizations'
+comment = '"Changes to basic visualizations Notebook"'
+noOfCommits=7
+
 def read_dates():
     f = open("Dates.txt", "r")
     for x in f:
@@ -12,20 +20,19 @@ read_dates()
 os.chdir(r"E:\Personal\Repositories\KaggleNotebooks")
 # subprocess.check_call(['git'] + ['status'])
 
-for i in range(0, 21):
+for i in range(0, noOfCommits):
     if i == 0:
         add = '';
     else:
         add = ' (' + str(i) + ')'
 
-    filename = 'beginners-basic-workflow-introduction' + add + '.ipynb'
+    filename = fileTitle + add + '.ipynb'
 
     cmd = 'copy "E:\Personal\Repositories\PythonTools\GitCommiter\\res\\' + filename + '" ' + \
-          '"E:\Personal\Repositories\KaggleNotebooks\BasicWorkflowIntroduction' \
-          '\\beginners-basic-workflow-introduction.ipynb"'
+          '"E:\Personal\Repositories\KaggleNotebooks\\' + folderName + '\\' + fileTitle + '.ipynb"'
     print(cmd)
     os.system(cmd)
     subprocess.check_call(['git'] + ['add', '*'])
-    subprocess.check_call(['git'] + ['commit', '-m', r'"Changes to Basic WorkFlow Notebook"'])
-    dateComplete = '--date="' + dateList[i] +'"'
+    subprocess.check_call(['git'] + ['commit', '-m', comment])
+    dateComplete = '--date="' + dateList[i] + '"'
     subprocess.check_call(['git'] + ['commit', '--amend', '--no-edit', dateComplete])
