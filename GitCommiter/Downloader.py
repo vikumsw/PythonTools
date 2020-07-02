81f0d734c5816a77d5f38b3961be335c7504b68d
Vikum Wijesinghe <vikumsw@gmail.com>
2020-07-02 19:38:05 +0530

    Included downloader to main file
    Signed-off-by: Vikum Wijesinghe <vikumw@lseg.com>

--- GitCommiter/Downloader.py
+++ GitCommiter/Downloader.py
@@ -2,13 +2,12 @@ import requests
 
 # 'https://www.kaggle.com/kernels/scriptcontent/14481387/download'
 
-l = [16723308,16723379,16772145,16817530,17623070,17974938,17975477]
 
-fileTitle='basic-feature-visualizations'
+def download(file_title, l):
+    for i, k in zip(range(30), l):
+        url = 'https://www.kaggle.com/kernels/scriptcontent/' + str(k) + '/download'
+        r = requests.get(url, allow_redirects=True)
+        filename = 'res/' + file_title + '(' + str(i) + ').ipynb'
+        print(url, filename)
+        open(filename, 'wb').write(r.content)
 
-for i,k in zip(range(30), l):
-    url = 'https://www.kaggle.com/kernels/scriptcontent/' + str(k) +'/download'
-    r = requests.get(url, allow_redirects=True)
-    filename = fileTitle + ' (' + str(i) + ').ipynb'
-    print(url, filename)
-    open(filename, 'wb').write(r.content)
--- GitCommiter/git.py
+++ GitCommiter/git.py
@@ -1,10 +1,17 @@
 import os
 import subprocess
+from Downloader import download
 
-folderName = 'BasicFeatureVisualizations'
-fileTitle='basic-feature-visualizations'
-comment = '"Changes to basic visualizations Notebook"'
-noOfCommits=7
+folderName = 'ExplainingRandomForestModelWithLime'
+fileTitle='explaining-random-forest-model-with-lime'
+comment = '"Changes to explaining-random-forest-model-with-lime Notebook"'
+noOfCommits = 3
+repetition = 2
+
+# Download tags
+ll = [17598324, 17608560, 17610515]
+#done
+
 
 def read_dates():
     f = open("Dates.txt", "r")
@@ -16,23 +23,24 @@ def read_dates():
 dateList = list()
 read_dates()
 
+download(fileTitle, ll)
 
 os.chdir(r"E:\Personal\Repositories\KaggleNotebooks")
-# subprocess.check_call(['git'] + ['status'])
+os.system("mkdir " + folderName)
 
-for i in range(0, noOfCommits):
-    if i == 0:
-        add = '';
-    else:
-        add = ' (' + str(i) + ')'
+d = 0
+for k in range(0, repetition):
+    for i in range(0, noOfCommits):
+        add = '(' + str(i) + ')'
 
-    filename = fileTitle + add + '.ipynb'
+        filename = fileTitle + add + '.ipynb'
 
-    cmd = 'copy "E:\Personal\Repositories\PythonTools\GitCommiter\\res\\' + filename + '" ' + \
-          '"E:\Personal\Repositories\KaggleNotebooks\\' + folderName + '\\' + fileTitle + '.ipynb"'
-    print(cmd)
-    os.system(cmd)
-    subprocess.check_call(['git'] + ['add', '*'])
-    subprocess.check_call(['git'] + ['commit', '-m', comment])
-    dateComplete = '--date="' + dateList[i] + '"'
-    subprocess.check_call(['git'] + ['commit', '--amend', '--no-edit', dateComplete])
+        cmd = 'copy "E:\Personal\Repositories\PythonTools\GitCommiter\\res\\' + filename + '" ' + \
+              '"E:\Personal\Repositories\KaggleNotebooks\\' + folderName + '\\' + fileTitle + '.ipynb"'
+        print(cmd)
+        os.system(cmd)
+        subprocess.check_call(['git'] + ['add', '*'])
+        subprocess.check_call(['git'] + ['commit', '-m', comment])
+        dateComplete = '--date="' + dateList[d] + '"'
+        d = d+1
+        subprocess.check_call(['git'] + ['commit', '--amend', '--no-edit', dateComplete])
